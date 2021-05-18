7e9034bcae839c0c0216ce86123786f41d050deb
instantflorian <info@meerwinck.com>
2021-05-18 06:45:07 +0200

    Avoid duplicates in FE and make tabs working in BE

--- view.php
+++ view.php
@@ -75,99 +75,106 @@ list($vars,$default_replacements) = mod_nwi_replacements();
 $page_keywords = array();
 
 // ----- read post ---------------------------------------------------------------
-if (defined('POST_ID') && is_numeric(POST_ID)) {
 
-    if(!$page_link) {
-        exit($MESSAGE['PAGES']['NOT_FOUND']);
-    }
 
-    // for functions that use global $section_id
-    $section_id = $post_section;
 
-    // tags
-    $tags = mod_nwi_get_tags_for_post(POST_ID);
-    foreach ($tags as $i => $tag) {
-        $tags[$i] = "<span class=\"mod_nwi_tag\" id=\"mod_nwi_tag_".POST_ID."_".$i."\""
-                  . (!empty($tag['tag_color']) ? " style=\"background-color:".$tag['tag_color']."\"" : "" ) .">"
-                  . "<a href=\"".$wb->page_link(PAGE_ID)."?tags=".$tag['tag']."\">".$tag['tag']."</a></span>";
-        if(!isset($page_keywords[$tag['tag']])) {
-            $page_keywords[] = htmlspecialchars($tag['tag'], ENT_QUOTES | ENT_HTML401);
-        }
-    }
+if (defined('POST_ID') && is_numeric(POST_ID) ) {
+	
+	if (intval($post_section) == intval($section_id)) {
+		
+		if(!$page_link) {
+			exit($MESSAGE['PAGES']['NOT_FOUND']);
+		}
 
-    $post = mod_nwi_post_show(intval(POST_ID));
-    $images = mod_nwi_img_get_by_post(intval(POST_ID),true);
+		// for functions that use global $section_id
+		$section_id = $post_section;
 
-    $replacements = array_merge(
-        $default_replacements,
-        $TEXT,
-        array_change_key_case($post,CASE_UPPER),
-        $MOD_NEWS_IMG,
-        array(
-            'IMAGE'           => $post['post_img'],
-			'IMAGE_URL' 	  => WB_URL.MEDIA_DIRECTORY.'/.news_img/'.$post['image'],
-            'IMAGES'          => implode("", $images),
-            'SHORT'           => $post['content_short'],
-            'LINK'            => $post['post_link'],
-            'MODI_DATE'       => $post['post_date'],
-            'MODI_TIME'       => $post['post_time'],
-            'PAGE_TITLE'      => (strlen($page['page_title']) ? $page['page_title'] : $page['menu_title']),
-            'TAGS'            => implode(" ", $tags),
-            'CONTENT'         => $post['content_short'].$post['content_long'],
-            'BACK'            => $page_link,
-            'PREVIOUS_PAGE_LINK'
-                => (strlen($post['prev_link'])>0 ? '<a href="'.$post['prev_link'].'">'.$MOD_NEWS_IMG['TEXT_PREV_POST'].'</a>' : null),
-            'NEXT_PAGE_LINK'
-                => (strlen($post['next_link'])>0 ? '<a href="'.$post['next_link'].'">'.$MOD_NEWS_IMG['TEXT_NEXT_POST'].'</a>' : null),
-            'DISPLAY_PREVIOUS_NEXT_LINKS'
-                => ((strlen($post['prev_link'])>0 || strlen($post['next_link'])>0) ? 'visible' : 'hidden'),
-        )
-    );
+		// tags
+		$tags = mod_nwi_get_tags_for_post(POST_ID);
+		foreach ($tags as $i => $tag) {
+			$tags[$i] = "<span class=\"mod_nwi_tag\" id=\"mod_nwi_tag_".POST_ID."_".$i."\""
+					  . (!empty($tag['tag_color']) ? " style=\"background-color:".$tag['tag_color']."\"" : "" ) .">"
+					  . "<a href=\"".$wb->page_link(PAGE_ID)."?tags=".$tag['tag']."\">".$tag['tag']."</a></span>";
+			if(!isset($page_keywords[$tag['tag']])) {
+				$page_keywords[] = htmlspecialchars($tag['tag'], ENT_QUOTES | ENT_HTML401);
+			}
+		}
 
-    // use block 2
-    $post_block2 = '';
-    if ($settings['use_second_block']=='Y') {
-        // get content from post
-        $post_block2 = ($post['content_block2']);
-        if (empty($post_block2) && !empty($settings['block2'])) {
-            // get content from settings
-            $post_block2 = $settings['block2'];
-        }
-        // replace placeholders
-        $post_block2 = preg_replace_callback(
-            '~\[('.implode('|',$vars).')+\]~',
-            function($match) use($replacements) {
-                return (isset($match[1]) && isset($replacements[$match[1]]))
-                    ? $replacements[$match[1]]
-                    : '';
-            },
-            $post_block2
-        );
-        if (!defined("MODULES_BLOCK2")) {
-            define("MODULES_BLOCK2", $post_block2);
-        }
-        if(!defined("NEWS_BLOCK2")) {
-            define("NEWS_BLOCK2", $post_block2);
-        }
-        if(!defined("TOPIC_BLOCK2")) {
-            define("TOPIC_BLOCK2", $post_block2);
-        }
-    }
+		$post = mod_nwi_post_show(intval(POST_ID));
+		$images = mod_nwi_img_get_by_post(intval(POST_ID),true);
 
-    $output = preg_replace_callback(
-        '~\[('.implode('|',$vars).')+\]~',
-        function($match) use($replacements) {
-            return (isset($match[1]) && isset($replacements[$match[1]]))
-                ? $replacements[$match[1]]
-                : '';
-        },
-        $settings['post_header'].$settings['post_content'].$settings['post_footer']
-    );
+		$replacements = array_merge(
+			$default_replacements,
+			$TEXT,
+			array_change_key_case($post,CASE_UPPER),
+			$MOD_NEWS_IMG,
+			array(
+				'IMAGE'           => $post['post_img'],
+				'IMAGE_URL' 	  => WB_URL.MEDIA_DIRECTORY.'/.news_img/'.$post['image'],
+				'IMAGES'          => implode("", $images),
+				'SHORT'           => $post['content_short'],
+				'LINK'            => $post['post_link'],
+				'MODI_DATE'       => $post['post_date'],
+				'MODI_TIME'       => $post['post_time'],
+				'PAGE_TITLE'      => (strlen($page['page_title']) ? $page['page_title'] : $page['menu_title']),
+				'TAGS'            => implode(" ", $tags),
+				'CONTENT'         => $post['content_short'].$post['content_long'],
+				'BACK'            => $page_link,
+				'PREVIOUS_PAGE_LINK'
+					=> (strlen($post['prev_link'])>0 ? '<a href="'.$post['prev_link'].'">'.$MOD_NEWS_IMG['TEXT_PREV_POST'].'</a>' : null),
+				'NEXT_PAGE_LINK'
+					=> (strlen($post['next_link'])>0 ? '<a href="'.$post['next_link'].'">'.$MOD_NEWS_IMG['TEXT_NEXT_POST'].'</a>' : null),
+				'DISPLAY_PREVIOUS_NEXT_LINKS'
+					=> ((strlen($post['prev_link'])>0 || strlen($post['next_link'])>0) ? 'visible' : 'hidden'),
+			)
+		);
 
-    // include gallery template
-    if (strlen($settings['gallery'])) {
-        include __DIR__.'/js/galleries/'.$settings['gallery'].'/include.tpl';
-    }
+		// use block 2
+		$post_block2 = '';
+		if ($settings['use_second_block']=='Y') {
+			// get content from post
+			$post_block2 = ($post['content_block2']);
+			if (empty($post_block2) && !empty($settings['block2'])) {
+				// get content from settings
+				$post_block2 = $settings['block2'];
+			}
+			// replace placeholders
+			$post_block2 = preg_replace_callback(
+				'~\[('.implode('|',$vars).')+\]~',
+				function($match) use($replacements) {
+					return (isset($match[1]) && isset($replacements[$match[1]]))
+						? $replacements[$match[1]]
+						: '';
+				},
+				$post_block2
+			);
+			if (!defined("MODULES_BLOCK2")) {
+				define("MODULES_BLOCK2", $post_block2);
+			}
+			if(!defined("NEWS_BLOCK2")) {
+				define("NEWS_BLOCK2", $post_block2);
+			}
+			if(!defined("TOPIC_BLOCK2")) {
+				define("TOPIC_BLOCK2", $post_block2);
+			}
+		}
+
+		$output = preg_replace_callback(
+			'~\[('.implode('|',$vars).')+\]~',
+			function($match) use($replacements) {
+				return (isset($match[1]) && isset($replacements[$match[1]]))
+					? $replacements[$match[1]]
+					: '';
+			},
+			$settings['post_header'].$settings['post_content'].$settings['post_footer']
+		);
+
+		// include gallery template
+		if (strlen($settings['gallery'])) {
+			include __DIR__.'/js/galleries/'.$settings['gallery'].'/include.tpl';
+		}
+		
+	} // check Spost_section == $section_id
 
 // ----- list of posts -----------------------------------------------------------
 } else {
