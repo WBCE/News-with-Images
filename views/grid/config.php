01eddf352f3af13a6aaf7e62a6acabb152c5231d
Bianka Martinovic <github.20.cerberus@spamgourmet.com>
2026-06-19 19:08:38 +0200

    news_img: grid-View aufhuebschen (Karten, Tags, Button, No-Image, Navi)
    Die grid-View war bis auf das Raster praktisch ungestylt, weil jede View nur
    ihre eigene frontend.css laedt und nichts von default erbt. Listenansicht
    und Leseansicht durchgaengig auf den Stand der default-View gebracht.
    
    views/grid/frontend.css (neu aufgebaut):
    - Beitraege als echte Karten: weisser Hintergrund, dezenter Rahmen (#e2e8f0),
      abgerundet, Bild randlos oben; Karten gleich hoch via Flex-Spalte
    - Bildbereich mit fester aspect-ratio 3/2 + object-fit cover -> gleiche
      Header-Hoehe mit Bild UND mit Platzhalter-Kachel (behebt den schmaleren
      Header bei Beitraegen ohne Bild)
    - Tags als Navy-Pills und Weiterlesen-Button als Outline-Button mit CSS-Pfeil
      (identisch zur default-View)
    - unterer Kartenbereich gestapelt: erst Tags, dann Button in eigener Zeile
      darunter (linksbuendig) - sieht in schmalen Kacheln besser aus
    - Tag-Abstand getrennt: margin-top fuer die Detailansicht (Abstand zur
      Navigation), in der Listen-Kachel via .mod_nwi_bottom auf 0
    - Lead-/Teaser-Box der Leseansicht (.mod_nwi_content_short) uebernommen
    - Pagination/Navigation als neutrale Flex-<nav> (statt <table>)
    - responsive: unter 600px immer einspaltig
    
    views/grid/config.php:
    - $footer und $post_footer von <table> auf <nav class="mod_nwi_nav">
      umgestellt (drei gleich breite Zonen, Mitte zentriert)

--- views/grid/config.php
+++ views/grid/config.php
@@ -21,13 +21,11 @@ $post_loop = '<section class="mod_nwi_grid_box">
 </section>';
 $footer = '</div>
 <div class="mod_nwi_spacer"></div>
-<table class="mod_nwi_table" style="visibility:[DISPLAY_PREVIOUS_NEXT_LINKS]">
-<tr>
-    <td class="mod_nwi_table_left">[PREVIOUS_PAGE_LINK]</td>
-    <td class="mod_nwi_table_center">[OF]</td>
-    <td class="mod_nwi_table_right">[NEXT_PAGE_LINK]</td>
-</tr>
-</table>';
+<nav class="mod_nwi_nav" style="visibility:[DISPLAY_PREVIOUS_NEXT_LINKS]">
+    <div class="mod_nwi_nav_left">[PREVIOUS_PAGE_LINK]</div>
+    <div class="mod_nwi_nav_center">[OF]</div>
+    <div class="mod_nwi_nav_right">[NEXT_PAGE_LINK]</div>
+</nav>';
 
 $block2 = '';
 $post_header = addslashes('<h2>[TITLE]</h2>
@@ -43,11 +41,9 @@ $post_content = '<div class="mod_nwi_content_short">
 ';
 $image_loop = '<img src="[IMAGE]" alt="[DESCRIPTION]" title="[DESCRIPTION]" data-caption="[DESCRIPTION]" />';
 $post_footer = ' <div class="mod_nwi_spacer"></div>
-<table class="mod_nwi_table" style="visibility: [DISPLAY_PREVIOUS_NEXT_LINKS]">
-<tr>
-    <td class="mod_nwi_table_left">[PREVIOUS_PAGE_LINK]</td>
-    <td class="mod_nwi_table_center"><a href="[BACK]">[TEXT_BACK]</a></td>
-    <td class="mod_nwi_table_right">[NEXT_PAGE_LINK]</td>
-</tr>
-</table>
+<nav class="mod_nwi_nav" style="visibility: [DISPLAY_PREVIOUS_NEXT_LINKS]">
+    <div class="mod_nwi_nav_left">[PREVIOUS_PAGE_LINK]</div>
+    <div class="mod_nwi_nav_center"><a href="[BACK]">[TEXT_BACK]</a></div>
+    <div class="mod_nwi_nav_right">[NEXT_PAGE_LINK]</div>
+</nav>
 <div class="mod_nwi_tags">[TAGS]</div>';
